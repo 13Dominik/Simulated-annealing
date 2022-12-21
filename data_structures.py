bfd8e21251cf4e329db8f047db519b43ea027c91
13Dominik <dtomalczyk@student.agh.edu.pl>
2022-12-21 01:09:20 +0100

    init solution function seems to work, some fixes, now we should write a new_solution function, to find solution in neighboour

--- data_structures.py
+++ data_structures.py
@@ -29,6 +29,7 @@ class Car:
         :param ave_fuel_consumption: Average fuel consumption
         :param curr_position: Current position of car at road
         :param curr_fuel_level: Current amount of fuel in tank
+        :param curr_level_at_steps: Contains fuel level in tank at every route stage (station position)
         :return: None
         """
         self.tank_capacity = tank_capacity
@@ -37,34 +38,39 @@ class Car:
         self.curr_fuel_level = curr_fuel_level
         self.fuel_level_at_steps = [self.curr_fuel_level]
 
-    def move_car(self, station_amount: Tuple[Station, int]):
+    def move_car(self, station: Station) -> float:
         """
-        Method which update car position, and current fuel level
-        :param station_amount: Station where we will tank, and amount of fuel we will tank
-        :return:
+        Method which update car position, current fuel level, and fuel level at steps
+        :param station: Station where we will tank, we are tanking as much as possible
+        :return: Amount of fuel we are tanking on this particular station
         """
-        station, fuel_amount = station_amount
+        # station, fuel_amount = station_amount
 
         # Computing how much fuel will we have when we arrive station
-        self.curr_fuel_level = self.curr_fuel_level - (station.road_position - self.curr_position + station.extra_route) / 100 * self.ave_fuel_consumption
-        # Adding fuel which we tank
-        self.curr_fuel_level += fuel_amount
+        current_lack = self.tank_capacity - self.curr_fuel_level  # current space in tank
+        fuel_we_will_use = (station.road_position - self.curr_position + station.extra_route) / 100 * self.ave_fuel_consumption
+        # current_lack + amount of fuel we will use to get in to the station
+        amount_to_tank = fuel_we_will_use + current_lack
+
+        # Tanking as much as possible (tank must be full)
+        self.curr_fuel_level = self.tank_capacity  # tank max
         # Computing how much will we have when we back at road from station
         self.curr_fuel_level = self.curr_fuel_level - station.extra_route / 100 * self.ave_fuel_consumption
         self.curr_position = station.road_position
         self.fuel_level_at_steps.append(self.curr_fuel_level)
+        return amount_to_tank
 
 
 class Solution:
 
     def __init__(self, car: Car, solution=None):
         """
-        :param vector: Vector of stations and amount of petrol we should buy, for instance:
+        :param solution: Vector of stations and amount of petrol we should buy, for instance:
         [(A, 10), (B, 21), (C, 234)], if None it will be a empty list and we can add
         """
         self.solution = solution
         self.car = car
-        self.penalty_function = 0
+        self.penalty_function = []
 
     @property
     def solution(self):
@@ -77,32 +83,25 @@ class Solution:
         else:
             self.__solution = value
 
-    def add_station(self, station_amount: Tuple[Station, int]) -> None:
+    def add_station(self, station_amount: Tuple[Station, float]) -> None:
         """
 
         Add new station and amount of petrol bought eg. ("A1", 20) and  update car info
         :param station_amount:
         :return:
         """
-        #TODO pasuje jeszcze dodać do postaci rozwiązania(lub inaczej to zapisać) informacje o ilości paliwa po zatankowaniu
+        # TODO pasuje jeszcze dodać do postaci rozwiązania(lub inaczej to zapisać) informacje o ilości paliwa po zatankowaniu
         # i wróceniu na trase. Wtedy Jak mamy stacje A -> B -> C i wyrzucimy B, to przsuwamy car na A, i możemy mu ustawić dzięki
         # temu car.curr_fuel_level. car.curr_position ustawaimy mu wtedy jako B.road_position.
 
         self.__solution.append(station_amount)
-        # Updating information about car
-        self.car.move_car(station_amount)
         # Updating penalty function, if we tank less than half of tank capacity
         if station_amount[1] < self.car.tank_capacity / 2:
-            #TODO Tutaj można pomyśleć nad wzorem, i trzeba pamiętać że jak wyrzucamy jakąś stację
-            # z rozwiązania to trzeba zmniejszyć funkcję kary - odejmiemy tylko od self.penalty_function
-            # drugą wartość z krotki którą wyrzucamy, zgodnie z poniższym wzorem
-
             # The less we tank at once time, the more we punish
-            self.penalty_function += (self.car.tank_capacity / 2 - station_amount[1]) * 0.5
-
+            self.penalty_function.append((self.car.tank_capacity / 2 - station_amount[1]) * 0.5)
 
     def solution_value(self) -> float:
-        return sum([station[0].price * station[1] for station in self.solution]) + self.penalty_function
+        return sum([station[0].price * station[1] for station in self.solution]) + sum(self.penalty_function)
 
     def __iter__(self):
         return iter(self.solution)
--- sa_algorithm.py
+++ sa_algorithm.py
@@ -1,12 +1,27 @@
 import random
 from math import exp
+from typing import Callable, List
+
+from utils import list_of_possible_station
 from data_structures import Solution, Station, Car
-from typing import Callable
 
 
-def init_solution() -> Solution:
-    pass
-
+def init_solution(car: Car, end_point: int, stations: List[Station]) -> Solution:
+    """
+    :param car: Car that we are traveling
+    :param end_point: Point (int coordinate) which represents end of route
+    :param stations: List of stations
+    :return: Solution (random)
+    """
+    solution = Solution(car)
+    #     car position + (fuel we have in tank) - reserve < end_point
+    while car.curr_position + car.curr_fuel_level / car.ave_fuel_consumption * 100 - 10 < end_point:
+        stations_in_step = list_of_possible_station(car, stations)
+        station = random.choice(stations_in_step)
+        amount = car.move_car(station)
+        solution.add_station((station, amount))
+    # TODO: Uzupełnić o to jak dużo nam zostało paliwa i jak to wliczać w solution
+    return solution
 
 def new_solution() -> Solution:
     pass
@@ -23,7 +38,7 @@ def simulated_annealing(new_solution: Callable, init_solution: Solution, T: int,
     """
     :param new_solution: Function that returns new solution to compare
     :param init_solution: First solution
-    :param T: Temperature parametr #TODO: Tu sie trzeba zastanowic jak to ustawic
+    :param T: Temperature parametr
     :param alfa: Parameter to reduce T
     :param iter_max: Maximum number of iterations (needed in stop condition)
     :return: Solution
@@ -46,11 +61,14 @@ def simulated_annealing(new_solution: Callable, init_solution: Solution, T: int,
     return solution
 
 # Wszystko ponizej tylko do testu
-# c1 = Car(10, 20, 0, 0)
-# s1 = Station("A", 10, 0, 0)
-# s2 = Station("B", 20, 0, 0)
-# s3 = Station("C", 30, 0, 0)
-# print(simulated_annealing(new_solution, Solution(c1, [(s1, 10), (s2, 20), (s3, 30)]), 200, 0.95))
-#
-# print(new_solution())
-# print(Solution(c1, [(s1, 5), (s2, 20), (s3, 30)]))
+c1 = Car(200, 10, 0, 50)
+s1 = Station("A", 3, 20, 200)
+s2 = Station("B", 3.5, 25, 190)
+s3 = Station("C", 4, 15, 210)
+s4 = Station("D", 4, 30, 650)
+s5 = Station("E", 4, 40, 750)
+stations = [s1, s2, s3, s4, s5]
+
+
+sol = init_solution(c1, 2500, stations)
+print(sol)
